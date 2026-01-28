547f77a2298c503ae63eb3c21b6af1fadf5a7ec2
Bartłomiej Sokulski <bartlomiej.sokulski777@gmail.com>
2026-01-28 09:25:46 +0100

    update strony, css js i dzwiek lobotomii

--- public/script.js
+++ public/script.js
@@ -8,3 +8,13 @@ function playAudio() {
         origin: { y: 0.6 }
     });
 }
+function lobo() {
+    const audio = document.getElementById("lobo");
+    audio.play();
+
+    confetti({
+        particleCount: 150,
+        spread: 70,
+        origin: { y: 0.6 }
+    });
+}
